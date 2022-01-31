4c1ecffb9210ca088a5a7fb15d9e4744fc8d82e3
Tomoya Nakada <e215725@ie.u-ryukyu.ac.jp>
2022-02-01 00:16:10 +0900

    Change example

--- Qsort.c
+++ Qsort.c
@@ -10,29 +10,29 @@ void QSort(int scores[], int left, int right)
     int i, j;
     int pivot;
 
-    i = left;                      /* ソートする配列の一番小さい要素の添字 */
-    j = right;                     /* ソートする配列の一番大きい要素の添字 */
+    i = left;                           /* ソートする配列の一番小さい要素の添字 */
+    j = right;                          /* ソートする配列の一番大きい要素の添字 */
 
     pivot = scores[(left + right) / 2]; /* 基準値を配列の中央付近にとる */
 
-    while (1) {                    /* 無限ループ */
+    while (1) {                    
 
         while (scores[i] < pivot)       /* pivot より大きい値が */
-            i++;                   /* 出るまで i を増加させる */
+            i++;                        /* 出るまで i を増加させる */
 
         while (pivot < scores[j])       /* pivot より小さい値が */
-            j--;                   /*  出るまで j を減少させる */
-        if (i >= j)                /* i >= j なら */
-            break;                 /* 無限ループから抜ける */
+            j--;                        /*  出るまで j を減少させる */
+        if (i >= j)                     /* i >= j なら */
+            break;                 
 
         Swap(scores, i, j);             /* x[i] と x[j]を交換 */
-        i++;                       /* 次のデータ */
+        i++;                            /* 次のデータ */
         j--;
     }
 
-    if (left < i - 1)              /* 基準値の左に 2 以上要素があれば */
+    if (left < i - 1)                   /* 基準値の左に 2 以上要素があれば */
         QSort(scores, left, i - 1);     /* 左の配列を Q ソートする */
-    if (j + 1 <  right)            /* 基準値の右に 2 以上要素があれば */
+    if (j + 1 <  right)                 /* 基準値の右に 2 以上要素があれば */
         QSort(scores, j + 1, right);    /* 右の配列を Q ソートする */
 }
 
@@ -69,16 +69,6 @@ int main()
     int scores3[OTHER_SIZE] = {100, 60, 70, 100, 90, 80};
     QSort(scores3, 0, OTHER_SIZE - 1);
     showData(scores3, OTHER_SIZE);
-
-    int scores4[SIZE] = {0, 80, 60, 100, 60};
-    printf("データサイズ: %d\n", SIZE);
-    QSort(scores4, 0, SIZE - 1);
-    showData(scores4, SIZE);
-
-    printf("データサイズ: %d\n",OTHER_SIZE);
-    int scores5[OTHER_SIZE] = {10, 20, 30, 40, 50, 60};
-    QSort(scores5, 0, OTHER_SIZE - 1);
-    showData(scores5, OTHER_SIZE);
 
     return 0;
 }
